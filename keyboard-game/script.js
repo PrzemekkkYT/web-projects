d1773c837b27852cd36b0d67914d58e5746a8157
Przemekkk <42695398+PrzemekkkYT@users.noreply.github.com>
2021-10-05 21:38:26 +0200

    timer i gameover

--- keyboard-game/script.js
+++ keyboard-game/script.js
@@ -4,11 +4,23 @@ var kg_cells = document.getElementsByClassName("kg-cell");
 var selectedCell = 0;
 var treasureCell = 0;
 
+var timerSpeed = 1;
+var timerWidthStart = 1000;
+var timerWidth = 1000;
+var timerRunning = false;
+var gameover = false;
+
+
 function newTreasureCell(optSize) {
     treasureCell = Math.floor(Math.random() * ((Math.pow(optSize, 2)-1) - 0)) + 0;
     if (treasureCell == selectedCell) {
         newTreasureCell(optSize);
     }
+    timerWidth += (timerWidthStart/10)*Math.sqrt(timerSpeed);
+    if (timerWidth>timerWidthStart) {
+        timerWidth = timerWidthStart;
+    }
+    timerSpeed = parseInt(points.innerHTML)/10+1;
     console.log("treasureCell: "+treasureCell.toString());
 }
 
@@ -54,6 +66,12 @@ function keyDown(e) {
                 selectCell(selectedCell-optionSize+1);
             }
             break;
+        case "KeyP":
+            timerSpeed+=1;
+            break;
+        case "KeyO":
+            timerSpeed-=1;
+            break;
     }
     if (selectedCell == treasureCell) {
         points.innerHTML = parseInt(points.innerHTML) + 1;
@@ -77,6 +95,7 @@ function selected() {
     cells.style.width = ((mapSize.options[mapSize.selectedIndex].index+3)*120).toString();
     cells.style.height = ((mapSize.options[mapSize.selectedIndex].index+3)*120).toString();
     document.querySelectorAll('.kg-cell').forEach(e => e.remove());
+    document.querySelectorAll('.gameover').forEach(e => e.remove());
     console.log(Math.pow(mapSize.options[mapSize.selectedIndex].index+3, 2));
     for (var i=0; i<Math.pow(mapSize.options[mapSize.selectedIndex].index+3, 2); i++) {
         var el = document.createElement("div");
@@ -87,4 +106,49 @@ function selected() {
     newTreasureCell(mapSize.options[mapSize.selectedIndex].index+3);
     selectCell(Math.floor(Math.random() * ((Math.pow(mapSize.options[mapSize.selectedIndex].index+3, 2)-1) - 0)) + 0);
     points.innerHTML = 0;
+    timerWidth = timerWidthStart;
+    timerSpeed = 1;
+    gameover = false;
+    timerBar();
+}
+
+function timerBar() {
+    if(!timerRunning) {
+        timerWidth = timerWidthStart;
+        clearInterval(id);
+        var timerbar = document.getElementById("timerbar");
+        var id = setInterval(frame, 10);
+        function frame() {
+            if (timerWidth <= 0) {
+                clearInterval(id);
+                gameOver();
+            } else {
+                timerWidth-=timerSpeed;
+                timerbar.style.width = timerWidth/10 + "%";
+            }
+        }
+        timerRunning = true;
+    }
+}
+
+function reload() {
+    location.reload();
+}
+
+function gameOver() {
+    if (!gameover) {
+        document.querySelectorAll('.kg-cell').forEach(e => e.remove());
+        var el = document.createElement("a");
+        el.innerHTML = "GAME OVER\n\nKLIKNIJ ABY GRAĆ PONOWNIE";
+        el.className = "gameover";
+        el.href = "";
+        el.onclick = "reload()";
+        cells.appendChild(el);
+        var el2 = document.createElement("a");
+        el2.innerHTML = "Wynik: "+parseInt(points.innerHTML);
+        el2.className = "gameover";
+        cells.appendChild(el2);
+        timerRunning = false;
+        gameover = true;
+    }
 }
